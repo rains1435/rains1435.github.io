2464fccd163ca720974f6154ae274463d50ba170
rains1435 <rains6@nycstudents.net>
2024-09-27 14:04:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Rain S"; // update the part BETWEEN the "quotes"
+let userName = "rains1435"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
